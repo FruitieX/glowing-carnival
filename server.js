047f64d9544a6032da4aa3edaf89917331a2d4e8
Rasmus Eskola <fruitiex@gmail.com>
2016-01-30 04:02:03 +0200

    use browser-sync insteal of node-static for fancy auto reloads when modifying source!

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
-static = require('node-static');
+var bs;
+
+try {
+  bs = require('browser-sync').create();
+} catch(e) {
+  console.log('ERROR: failed to load the browser-sync module, forgot to run "npm install"?');
+  process.exit(0); // quit with success code so npm doesn't spew out a huge error message
+}
+
+bs.watch('*.html').on('change', bs.reload);
+bs.watch('js/*.js').on('change', bs.reload);
+
+var port = 8080;
+bs.init({
+  server: './',
+  port: port
+});
+
+// old node-static based solution
+/*
+var static = require('node-static');
 
 // disable caching so we don't have to worry about that while developing
 var fileServer = new static.Server('./', { cache: false });
@@ -10,3 +30,4 @@ require('http').createServer(function (request, response) {
     }).resume();
 }).listen(port);
 console.log('listening on: ' + port);
+*/
